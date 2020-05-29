8c33d21ba0a2b11c4bcc08ad591195b46fcb4762
ankit-rajpoot <ankitrajpoot@aartek.in>
2020-05-29 13:54:37 +0530

    create poc on how to select multiple files select in angular

---
+++ Angular/file-upload/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgxPaginationModule } from 'ngx-pagination'
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxPaginationModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
